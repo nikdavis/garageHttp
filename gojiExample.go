9f9ded0e76706cd2067ecbf3dc656cc20752bb30
Nikolas Davis <nikdavis@pdx.edu>
2017-12-02 23:34:46 -0800

    Basic goji webserver

---
+++ gojiExample.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "fmt"
+    "encoding/json"
+    "net/http"
+
+    "github.com/zenazn/goji"
+    "github.com/zenazn/goji/web"
+)
+
+type HelloWorld struct {
+    Hello string
+}
+
+type Error struct {
+    Status int
+    Error string
+}
+
+type Payload interface {
+
+}
+
+type Response struct {
+    Payload Payload
+    Error   *Error
+}
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+    response := Response{Payload: &HelloWorld{Hello: c.URLParams["name"]}}
+    var b []byte
+    var err error
+
+    if response.Error != nil {
+        w.WriteHeader(response.Error.Status)
+        b, _ = json.Marshal(*response.Error)
+    } else {
+        b, _ = json.Marshal(response.Payload)
+    }
+
+    if err != nil {
+        http.Error(w, "(╯°□°）╯︵ ┻━┻   woops, we lost our cool", http.StatusInternalServerError)
+        return
+    }
+
+    fmt.Fprint(w, string(b))
+}
+
+func main() {
+    goji.Get("/hello/:name", hello)
+    goji.Serve()
+}
